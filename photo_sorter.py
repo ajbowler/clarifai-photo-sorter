8134b09cc83c437b40cfe35bab4b1d4d4bd4b975
Andrew Bowler <bowlerandrewj@gmail.com>
2016-05-11 19:59:47 -0500

    Add extra printing for the user

--- photo_sorter.py
+++ photo_sorter.py
@@ -9,6 +9,7 @@ image_tag_lists = []
 clarifai_api = ClarifaiApi() # assumes environment variables are set.
 
 user_path = raw_input("Please enter path of photo library: ")
+print '\n'
 
 if not os.path.exists(user_path):
     print("Path does not exist")
@@ -16,22 +17,31 @@ if not os.path.exists(user_path):
 
 image_paths = os.listdir(user_path)
 
-for path in image_paths:
-    result = clarifai_api.tag_images(open(user_path + path, 'rb'))['results'][0]['result']['tag']['classes']
+print 'Begin processing images...\n'
+for path in range(0, len(image_paths)):
+    result = clarifai_api.tag_images(open(user_path + image_paths[path], 'rb'))['results'][0]['result']['tag']['classes']
     image_tag_lists.append(result)
     for tag in result:
         if (tag in tag_histogram):
             tag_histogram[tag] += 1
         else:
             tag_histogram[tag] = 1
+    cur = str(path + 1)
+    print cur + ' out of ' + str(len(image_paths)) + ' images processed...' 
 
+print '\n'
 tag_tuples = sorted(tag_histogram.items(), key=operator.itemgetter(1))
 top_five_tuples = tag_tuples[len(tag_tuples) - 5:len(tag_tuples)]
 top_five_tags = []
+print 'The top 5 tags from your photos are:\n'
 for i in top_five_tuples:
+    print i[0]
     top_five_tags.append(i[0])
 
-os.makedirs('album/misc/')
+if not os.path.exists('album/misc/'):
+    os.makedirs('album/misc/')
+
+print '\nCreating directories for each tag...\n'
 for tag in top_five_tags:
     dir_name = 'album/' + tag + '/'
     if not os.path.exists(dir_name):
@@ -48,4 +58,7 @@ for i in range(0, len(image_tag_lists)):
             break;
     if not found_dir:
         copy(str(src), str('album/misc/'))
+    cur = str(i + 1)
+    print cur + ' out of ' + str(len(image_tag_lists)) + ' images sorted...'
 
+print '\nAll images sorted. Check the album directory for your newly sorted images!\n'
